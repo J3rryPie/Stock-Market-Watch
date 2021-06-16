07f2da6ff2f1a5b428d9aed58c921f8d6da1e5d3
Aayush Kawathekar <aayush.kawathekar@gmail.com>
2021-06-16 17:37:41 +0530

    basic

---
+++ watchlist/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class script(models.Model):
+    code = models.CharField(max_length = 7)
+    price = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+    buy_quantity = models.IntegerField(default=0)
+    buy_price = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+    sell_quantity = models.IntegerField(default=0)
+    sell_price = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+    change = models.DecimalField(max_digits=7, decimal_places=2,default=0)
+    change_percentage = models.DecimalField(max_digits=5, decimal_places=2,default=0)
+    state = models.CharField(max_length = 2,default='un')
+
+
